a680c4e099840def2fdd8d98a55567a867af64b5
Aishwarya <aishwaryasaywan@gmail.com>
2025-05-22 12:27:30 +0530

    Added mapping between actor and movie

--- src/main/java/com/tyss/movie_management/dto/Movie.java
+++ src/main/java/com/tyss/movie_management/dto/Movie.java
@@ -1,29 +1,18 @@
-package com.tyss.movie_management.entity;
+package com.tyss.movie_management.dto;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Data
-@Entity
-@Table(name = "movies")
+@Builder
 public class Movie {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer movieId;
     private Integer imdbRating;
     private String industry;
     private Integer releaseYear;
     private String studio;
     private String title;
-
-    @ManyToMany(mappedBy = "movies")
-    private List<Actor> actors;
-
 }
--- src/main/java/com/tyss/movie_management/entity/Actor.java
+++ src/main/java/com/tyss/movie_management/entity/Actor.java
@@ -1,22 +1,31 @@
 package com.tyss.movie_management.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Data
 @Entity
+@Table(name="actors")
 public class Actor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer actorId;
     private String actorName;
     private Integer birthYear;
+
+
+    @JoinTable(
+            name = "movie_actor",
+            joinColumns =@JoinColumn(name="movie_id"),
+            inverseJoinColumns =@JoinColumn (name="actor_id")
+    )
+    @ManyToMany
+    private List<Movie> movies;
 
 }
